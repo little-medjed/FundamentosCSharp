3892cb0e18282dac0153d202b127822b9b06b303
Erika Mota Santana <erika.santana@fitbank.com.br>
2022-07-08 12:11:39 -0300

    Exercicio do temporizador

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Stopwatch
 {
@@ -6,7 +7,47 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
         }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("\nSTOPWATCH\n");
+
+            Console.WriteLine("Digite um s no final para segundos. Ex: 10s = 10 segundos");
+            Console.WriteLine("Digite um m no final para minutos. Ex: 10m = 10 minutos");
+            Console.WriteLine("x - Sair");
+            Console.Write("\nQuanto tempo? ");
+            string tempo = Console.ReadLine().ToLower();
+
+            if(tempo == "x") System.Environment.Exit(0);
+
+            char type = char.Parse(tempo.Substring(tempo.Length - 1,1).ToLower());
+            int time = int.Parse(tempo.Substring(0,tempo.Length - 1));
+
+            Temporizador(time, type);
+        }
+
+        static void Temporizador(int time, char type)
+        {
+            int currentTime = 0;
+            int multiplier = 1;
+
+            if(type == 'm') multiplier = 60;
+
+            while(currentTime != time)
+            {   
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000*multiplier);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Terminado!");
+            Thread.Sleep(1000);
+        }       
     }
 }
+
